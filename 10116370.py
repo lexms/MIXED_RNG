d46f5e97c8192638550114a4531752e9993b9a36
lexms <alexandermanuel.s@gmail.com>
2019-03-31 21:04:51 +0700

    fix variable name

--- 10116370.py
+++ 10116370.py
@@ -11,7 +11,7 @@ class GenerateRandomNumber:
         self.z0 = 10116370
         self.count = 300
 
-    def mix_RNG_formula(self, Zi_minus_1):
+    def mixed_RNG_formula(self, Zi_minus_1):
         a_pow_n = pow(self.a,self.n)
 
         temp_Z = (  ( a_pow_n * Zi_minus_1 )+( (a_pow_n-1)/(self.a-1) * self.c )  )
@@ -24,19 +24,19 @@ class GenerateRandomNumber:
     def cdf_result(self,u):
         return pow((3/2*u),(2/3))
 
-    def RNG_mix(self):
+    def mixed_RNG_list(self):
         _list = []
         for i in range(0,self.count):
             if i == 0:
                 #Zi-1 = Z0
-                zi = self.mix_RNG_formula(self.z0)
+                zi = self.mixed_RNG_formula(self.z0)
                 ui = self.uniform_generate(zi)
                 xi = self.cdf_result(ui)
 
                 _list.append({'Zi-1': self.z0, 'Zi':zi,'Ui': ui, 'Xi': xi})
             elif i > 0:
-                Zi_minus_1 = self.mix_RNG_formula(_list[i-1]['Zi-1']) 
-                zi = self.mix_RNG_formula(Zi_minus_1)
+                Zi_minus_1 = self.mixed_RNG_formula(_list[i-1]['Zi-1']) 
+                zi = self.mixed_RNG_formula(Zi_minus_1)
                 ui = self.uniform_generate(zi)
                 xi = self.cdf_result(ui)
 
@@ -45,7 +45,7 @@ class GenerateRandomNumber:
         return _list
 
     def RNG_print(self):
-        _list = self.RNG_mix()
+        _list = self.mixed_RNG_list()
         print('No.\t |     Zi-1      |      Zi       |       Ui      |       Xi      |')
         print('-----------------------------------------------------------------------')
         for i in range(0,len(_list)):
